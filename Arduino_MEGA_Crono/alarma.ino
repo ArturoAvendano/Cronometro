8a62103d75437709d39e013232d065acde189149
Arturo Avendaño <a.avendano04@ufromail.cl>
2017-07-04 01:55:07 -0400

    Update alarma.ino

--- Arduino_MEGA_Crono/alarma.ino
+++ Arduino_MEGA_Crono/alarma.ino
@@ -1,7 +1,7 @@
 ///En caso de usar buzzer se puede activar una alarma a cierta hora del dia con duración de un minuto
-void alarma(int hora, int mini, int seg){
+/*void alarma(int hora, int mini, int seg){
 if(ti.hour==hora && ti.min==mini && ti.sec==seg){
 tone(buzzpin,666,30000);    
 }
-}
+}*/
 
